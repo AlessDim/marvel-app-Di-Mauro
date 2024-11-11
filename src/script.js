3008c0e8fb1889177da3aa2b7a0a1c6b02b03464
Alessandra Di Mauro <alessandradimauro791@gmail.com4>
2024-11-11 21:29:04 +0100

    Update characters list with data

--- src/script.js
+++ src/script.js
@@ -1,13 +1,25 @@
+// src/script.js
+
 function getCharacters() {
     fetch('data/characters.json')
         .then(response => {
             if (!response.ok) {
-                throw new Error('Erreur de reseau');
+                throw new Error('Network response was not ok');
             }
             return response.json();
         })
         .then(data => {
-            console.log(data);
+            const charactersList = document.getElementById('characters');
+            
+            // Vide la liste avant de la remplir
+            charactersList.innerHTML = '';
+
+            // Ajoute un élément li pour chaque personnage
+            data.forEach(character => {
+                const li = document.createElement('li');
+                li.textContent = character.name; // Ou un autre attribut de personnage
+                charactersList.appendChild(li);
+            });
         })
         .catch(error => {
             console.error('Il y a eu un problème avec l\'opération fetch :', error);
