7a1a11b89d47ef0b2a79cad02a0e4f7041b9f2ad
AlessDim <alessandradimauro791@gmail.com>
2024-11-19 23:36:53 +0100

    Revert "added character details page (#2)" (#3)
    This reverts commit fd1bdc09cdefb6ca07cfcdee4a32e5b88ee8010b.

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,7 +2,6 @@ import Layout from "./Layout";
 import AboutPage from "./pages/AboutPage";
 import CharactersPage, { loader as charactersLoader } from './pages/CharactersPage';
 import ContactPage from "./pages/ContactPage";
-import CharacterDetailsPage, { loader as characterDetailLoader } from './pages/CharacterDetailsPage';
 
 const routes = [
     {
@@ -12,7 +11,6 @@ const routes = [
             { path: "/", element: <CharactersPage />, loader: charactersLoader},
             { path: "/about", element: <AboutPage /> },
             { path: "/contact", element: <ContactPage /> },
-            {path: "/character/:id", element: <CharacterDetailsPage />, loader: characterDetailLoader },
         ],
     },
 ];
