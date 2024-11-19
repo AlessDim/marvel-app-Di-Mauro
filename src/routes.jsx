fd1bdc09cdefb6ca07cfcdee4a32e5b88ee8010b
AlessDim <alessandradimauro791@gmail.com>
2024-11-19 23:36:33 +0100

    added character details page (#2)
    Co-authored-by: Alessandra Di Mauro <alessandradimauro791@gmail.com4>

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import Layout from "./Layout";
 import AboutPage from "./pages/AboutPage";
 import CharactersPage, { loader as charactersLoader } from './pages/CharactersPage';
 import ContactPage from "./pages/ContactPage";
+import CharacterDetailsPage, { loader as characterDetailLoader } from './pages/CharacterDetailsPage';
 
 const routes = [
     {
@@ -11,6 +12,7 @@ const routes = [
             { path: "/", element: <CharactersPage />, loader: charactersLoader},
             { path: "/about", element: <AboutPage /> },
             { path: "/contact", element: <ContactPage /> },
+            {path: "/character/:id", element: <CharacterDetailsPage />, loader: characterDetailLoader },
         ],
     },
 ];
